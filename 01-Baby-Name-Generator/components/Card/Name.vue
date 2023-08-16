8f3975f3620c39ff0e0313fbf7cf3ef188156366
Ziyad Mohamid Shokry <125437788+ZIYAD-MOHAMID@users.noreply.github.com>
2023-08-16 18:05:41 +0300

    first commit

---
+++ 01-Baby-Name-Generator/components/Card/Name.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<NameProps>();
+interface NameProps {
+  name: string;
+  index: number;
+}
+
+const emit = defineEmits(["remove"]);
+const removeName = () => {
+  emit("remove", props.index);
+};
+</script>
+
+<template>
+  <div class="card">
+    <h4 @click="removeName">{{ name }}</h4>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  background-color: #1b3c8a;
+  border-radius: 1rem;
+  color: #fff;
+  cursor: pointer;
+  width: 25%;
+}
+</style>
